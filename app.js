421097e67b68a22591134e05038cb7ba9683b446
vanshita-tilwani <msvtilwani@gmail.com>
2023-11-20 15:44:54 -0500

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
-import ModuleRoutes from "./Modules/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import AssignmentRoutes from "./assignments/routes.js";
 const app = express();
