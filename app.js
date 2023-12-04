3993170e01a9ba93435b8053df288afe65c38d6a
vanshita-tilwani <msvtilwani@gmail.com>
2023-12-04 15:43:52 -0500

    Assignment 6

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 import "dotenv/config";
+import session from "express-session";
 import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-import cors from "cors";
 import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
+import cors from "cors";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+   origin: "http://localhost:3000",
+}
+));
+
+  
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app)
 CourseRoutes(app);
