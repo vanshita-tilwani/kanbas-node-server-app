c6116b5a2e2434e609f2939427887941f77b0840
vanshita-tilwani <msvtilwani@gmail.com>
2023-12-12 18:00:52 -0500

    quiz schema changes

--- quizzes/schema.js
+++ quizzes/schema.js
@@ -4,6 +4,13 @@ const quizSchema = new mongoose.Schema({
     description: {type: String, required: true},
     course: { type: String, required: true },
     published: {type: Boolean},
+    points: {type:Number, required:true},
+    shuffleAnswers: {type:Boolean},
+    timeLimit: {type:Number, required:true},
+    multipleAttempts: {type:Boolean},
+    dueDate: {type:Date, required : true},
+    availableFrom: {type:Date, required : true},
+    availableUntil: {type:Date},
   },
   { collection: "quiz" });
 export default quizSchema;
